48fce1343fc90feb6956f3f7243ab9217cb0414c
Uziel vasquez <20243ds096@utez.edu.mx>
2026-03-23 00:21:16 -0600

    Agregación de LowerCase para convertir los textos en minusculas y se pueda importar de manera correcta.

--- src/components/UI/Badge/Badge.jsx
+++ src/components/UI/Badge/Badge.jsx
@@ -1,7 +1,7 @@
 import styles from "./Badge.module.css";
 
 const Badge = ({ label, variant = "default" }) => (
-  <span className={`${styles.badge} ${styles[variant]}`}>
+  <span className={`${styles.badge} ${styles[variant.toLowerCase()]}`}>
     {label}
   </span>
 );
