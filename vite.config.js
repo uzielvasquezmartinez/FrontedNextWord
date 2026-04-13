0da6bfeab956a7ebfbeb1d3ce8af8dfbf58d75f6
uziel vasquez <20243ds096@utez.edu.mx>
2026-04-13 03:24:08 -0600

    feat: implement full-stack student scheduling system with teacher discovery, reservation services, and API proxy configuration

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,26 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server : {
-    host : true
-     
+  server: {
+    host: true,
+    proxy: {
+      '/api': {
+        target: 'https://unpoetically-interramal-loren.ngrok-free.dev',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/ws': {
+        target: 'https://unpoetically-interramal-loren.ngrok-free.dev', // HTTPs inicial necesario para SockJS
+        ws: true,
+        changeOrigin: true,
+        secure: false,
+        headers: {
+          'ngrok-skip-browser-warning': '69420' 
+        }
+      }
+    }
   },
-   define: {
-    global: {}  // ← dentro de define
+  define: {
+    global: {}
   }
-   
 })
