daacfe9f7ecf381bc7b3b3e893432707e9b2b0d2
uziel vasquez <20243ds096@utez.edu.mx>
2026-04-15 05:06:43 -0600

    implementaciond de una variable

--- src/services/Api.js
+++ src/services/Api.js
@@ -35,7 +35,8 @@ api.interceptors.request.use(
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    const status = error.response?.status;
+    if (status === 401) {
       localStorage.removeItem("token");
       localStorage.removeItem("user");
       window.location.href = "/login";
